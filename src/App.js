52c8ba7de9f8cc5f3a614e3f9d7b071f2a881a8a
swasthikshetty10 <swasthikshetty10902@gmail.com>
2022-11-07 07:26:16 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { data } from "./data";
 
 function App() {
+  const [selected, setSelected] = useState({ id: 0, imageId: 0 });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="w-screen h-screen flex overflow-hidden">
+        <div className={`scrollbar h-screen overflow-y-scroll p-4 items-center flex flex-col gap-4  bg-red-200 lg:w-1/4 w-full`}>
+          {data.map((item, index) => <button
+            onClick={(e) => {
+              if (index + 1 === selected.id) { setSelected({ id: 0, imageId: 0 }); return }
+              setSelected({ id: index + 1, imageId: 0 })
+            }}
+            className={`${index + 1 === selected.id ? 'bg-sky-500' : 'bg-sky-200'}
+             w-full max-w-[10rem] bg-sky-200 px-3 py-2 rounded-md shadow-md font-semibold  
+             hover:scale-105 transition-transform duration-200`}>
+            {item.name}
+          </button>)}
+        </div>
+        <div className="scrollbar w-full h-screen lg:w-2/4 bg-blue-100 flex flex-col  overflow-y-scroll">
+          {selected.id === 0 && <p className="text-center p-4">Click on any subjects to show related images </p>}
+          {
+            selected.id !== 0 && data[selected.id - 1].images.map((item, index) => <div
+              className={`${selected.imageId === index + 1 ? 'bg-sky-300' : 'bg-slate-200'} h-full cursor-pointer p-4`}
+              onClick={() => {
+                if (selected.imageId === index + 1) { setSelected((prev) => ({ ...prev, imageId: 0 })); return }
+                setSelected((prev) => ({ ...prev, imageId: index + 1 }))
+              }} >
+              <img className="w-auto mx-auto h-full" src={item.url} />
+            </div>
+            )
+          }
+        </div>
+        <div className="w-full h-screen scrollbar overflow-y-scroll lg:w-1/4 bg-yellow-100 flex flex-col gap-4 p-4">
+          {/* links */}
+          {selected.id != 0 && selected.imageId === 0 && <p className="text-center p-4">Click on any images to show related links</p>}
+
+          {(selected.id !== 0 && selected.imageId !== 0) && data[selected.id - 1].images[selected.imageId - 1].links.map((item, index) => <a href={item} target="_blank" className="overflow-hidden bg-sky-200 px-3 py-2 rounded-md shadow-md font-semibold cursor-pointer  hover:scale-105 transition-transform duration-200   ">
+            {item}
+          </a>)}
+        </div>
+      </div>
+    </div >
   );
 }
 
 export default App;
+
---
+++ src/data.js
@@ -0,0 +1,61 @@
+export const data = [{
+    name: "Yamaha",
+    images: [
+        {
+            url: "https://global.yamaha-motor.com/news/2021/0519/img/pic_main.jpg",
+            links: ['https://www.yamaha-motor-india.com/', 'https://global.yamaha-motor.com/', 'https://global.yamaha-motor.com/news/2021/0519/img/pic_main.jpg']
+        },
+        {
+            url: "https://i.ytimg.com/vi/3zsT6VOeFdM/maxresdefault.jpg",
+            links: ['https://i.ytimg.com/vi/3zsT6VOeFdM/maxresdefault.jpg', 'https://global.yamaha-motor.com/']
+        },
+        {
+            url: "https://global.yamaha-motor.com/img/card_002.jpg",
+            links: ['https://www.yamaha-motor-india.com/', 'https://global.yamaha-motor.com/', 'https://global.yamaha-motor.com/img/card_002.jpg']
+        },
+        {
+            url: "https://bd.gaadicdn.com/processedimages/yamaha/mt-15-2-0/640X309/mt-15-2-062e4b1d700b63.jpg",
+            links: ['https://global.yamaha-motor.com/']
+        },
+        {
+            url: "https://bd.gaadicdn.com/processedimages/yamaha/aerox-155/640X309/aerox-155619b81eb2a3b4.jpg",
+            links: ['https://www.yamaha-motor-india.com/', 'https://global.yamaha-motor.com/']
+        },
+
+    ]
+},
+{
+    name: "Honda",
+    images: [
+        {
+            url: "https://images.hindustantimes.com/auto/img/2022/10/07/600x338/01_2024_Honda_Prologue_Styling_Reveal_1665125527500_1665125534098_1665125534098.jpg",
+            links: ["https://images.hindustantimes.com/auto/img/2022/10/07/600x338/01_2024_Honda_Prologue_Styling_Reveal_1665125527500_1665125534098_1665125534098.jpg", 'https://www.honda2wheelersindia.com/', 'https://www.honda2wheelersindia.com/assets/images/hero-banner/hero-banner-1.jpg']
+        },
+        {
+            url: "https://stimg.cardekho.com/images/carexteriorimages/630x420/Honda/WR-V/7665/1645419998984/front-left-side-47.jpg",
+            links: ['https://stimg.cardekho.com/images/carexteriorimages/630x420/Honda/WR-V/7665/1645419998984/front-left-side-47.jpg',]
+        },
+        {
+            url: "https://images.hindustantimes.com/auto/img/2022/10/08/600x338/Honda_Prologue_2_1665225880367_1665225911778_1665225911778.jpg",
+            links: ['https://www.honda2wheelersindia.com/',]
+        },
+        {
+            url: "https://stimg.cardekho.com/images/carexteriorimages/630x420/Honda/Amaze/8541/1629275024620/front-left-side-47.jpg",
+            links: ['https://www.honda2wheelersindia.com/',]
+        },
+
+    ]
+},
+{
+    name: "Suzuki",
+    images: [
+        {
+            url: "https://www.financialexpress.com/wp-content/uploads/2022/07/Suzuki-Jimny-Sierra-4Sport.jpg",
+            links: ['https://www.financialexpress.com/wp-content/uploads/2022/07/Suzuki-Jimny-Sierra-4Sport.jpg', 'https://www.suzuki.co.in/']
+        },
+        {
+            url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRCH-OXTRgHMlbMpFl4Jh650GzvzanND4TDoA&usqp=CAU",
+            links: ['https://www.suzuki.co.in/']
+        }]
+},
+]
