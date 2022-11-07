f35365c6145181c324fcd907c91994555bf55ef8
swasthikshetty10 <swasthikshetty10902@gmail.com>
2022-11-07 07:33:17 +0530

    fix: netlify err

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <div className="w-screen h-screen flex overflow-hidden">
         <div className={`scrollbar h-screen overflow-y-scroll p-4 items-center flex flex-col gap-4  bg-red-200 lg:w-1/4 w-full`}>
-          {data.map((item, index) => <button
+          {data.map((item, index) => <button key={index}
             onClick={(e) => {
               if (index + 1 === selected.id) { setSelected({ id: 0, imageId: 0 }); return }
               setSelected({ id: index + 1, imageId: 0 })
@@ -21,7 +21,7 @@ function App() {
         <div className="scrollbar w-full h-screen lg:w-2/4 bg-blue-100 flex flex-col  overflow-y-scroll">
           {selected.id === 0 && <p className="text-center p-4">Click on any subjects to show related images </p>}
           {
-            selected.id !== 0 && data[selected.id - 1].images.map((item, index) => <div
+            selected.id !== 0 && data[selected.id - 1].images.map((item, index) => <div key={index}
               className={`${selected.imageId === index + 1 ? 'bg-sky-300' : 'bg-slate-200'} h-full cursor-pointer p-4`}
               onClick={() => {
                 if (selected.imageId === index + 1) { setSelected((prev) => ({ ...prev, imageId: 0 })); return }
@@ -36,7 +36,7 @@ function App() {
           {/* links */}
           {selected.id != 0 && selected.imageId === 0 && <p className="text-center p-4">Click on any images to show related links</p>}
 
-          {(selected.id !== 0 && selected.imageId !== 0) && data[selected.id - 1].images[selected.imageId - 1].links.map((item, index) => <a href={item} target="_blank" className="overflow-hidden bg-sky-200 px-3 py-2 rounded-md shadow-md font-semibold cursor-pointer  hover:scale-105 transition-transform duration-200   ">
+          {(selected.id !== 0 && selected.imageId !== 0) && data[selected.id - 1].images[selected.imageId - 1].links.map((item, index) => <a key={index} href={item} target="_blank" className="overflow-hidden bg-sky-200 px-3 py-2 rounded-md shadow-md font-semibold cursor-pointer  hover:scale-105 transition-transform duration-200   ">
             {item}
           </a>)}
         </div>
